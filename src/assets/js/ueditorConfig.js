73154ed416b0d9afa6daaacc746e6ebc9543eb88
默认ID <1749987265@qq.com>
2021-01-22 12:14:05 +0800

    景点添加完成修改，关联产品列表未修改

--- src/assets/js/ueditorConfig.js
+++ src/assets/js/ueditorConfig.js
@@ -10,6 +10,6 @@ export default {
         serverUrl: "/travelbh/api/ueditor/exec",
         // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
         // UEDITOR_HOME_URL: "/travelbh/travelbh/UEditor/",
-        UEDITOR_HOME_URL: "/travelbh/travelbh/UEditor/",
+        UEDITOR_HOME_URL: "https://travel.gxucreate.com/travelbh/travelbh/UEditor/",
     },
 }
